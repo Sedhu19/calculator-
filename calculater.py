e2e9569c0475406158d70f1a827085619dcff12e
sedhu <104960685+Sedhu19@users.noreply.github.com>
2022-05-05 11:16:23 +0530

    Add files via upload

---
+++ calculater.py
@@ -0,0 +1,14 @@
+import math
+a = int(input("Enter a number : "))
+b = int(input("Enter another number : "))
+print("The Sum is ", a+b)
+print("The Difference is ", a-b)
+print("The product is ", a*b)
+print("The Division is ", a/b)
+print("The modulus is ", a%b)
+print("The a to the power of  b is ", a**b)
+print("The sine of a is ", math.sin(a))
+print("The cosine of a is ", math.cos(a))
+print("The tangent of a is ", math.tan(a))
+print("The square root of a is ", math.sqrt(a))
+
